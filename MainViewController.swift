617a456c41637551e212eeffe1ad8039db8c8874
hosxes <loli@h.fo>
2020-02-26 01:42:27 +0900

    Revert "Merge branch 'master' of https://github.com/hosxes/yixiang-ios"
    This reverts commit 66d9dc25ed0b36215f25ca0d6e45d445c119c174.

--- MainViewController.swift
+++ MainViewController.swift
@@ -7,23 +7,11 @@
 //
 
 import UIKit
-import WebKit
 
 class MainViewController: UIViewController {
-    
-    var webView: WKWebView!
 
-    @IBOutlet weak var contentView: WKWebView!
     override func viewDidLoad() {
-        if isIphoneX {
-            webView.scrollView.contentInsetAdjustmentBehavior = .never;
-        }
         super.viewDidLoad()
-        
-        if let url = NSURL(string: "http://localhost:3000/index") {
-            let request = NSURLRequest(url: url as URL)
-            contentView.load(request as URLRequest)
-        }
     }
 
 }
