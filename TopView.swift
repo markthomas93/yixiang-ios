3bc0b012fbde95aa41968d18adefbe1e7e9acbd1
hosxes <loli@h.fo>
2020-03-02 03:51:38 +0900

    tab bar design done

--- TopView.swift
+++ TopView.swift
@@ -8,58 +8,39 @@
 
 import SwiftUI
 
-var compass = "compass"
-var crown = "crown"
-var dashboard = "dashboard"
-var message = "message"
-var user = "user"
-
 struct TopView: View {
     @ObservedObject var viewRouter = ViewRouter()
+    @State private var selection = 0;
     var body: some View {
-        TabView {
-            Text("The First Tab")
+        TabView(selection: $selection) {
+            ContentView()
                 .tabItem {
-                    Image(self.viewRouter.currentView == compass ?  "compass-fill" : compass)
-                    Text("主页")
+                    Image(selection == 1 ? "compass-fill" : "compass")
+                    Text("主页").foregroundColor(.black)
                 }.tag(1)
-                .onTapGesture {
-                    self.viewRouter.currentView = compass
-            }
             Text("Another Tab")
                 .tabItem {
-                    Image(dashboard)
+                    Image(selection == 2 ? "dashboard-fill" : "dashboard")
                     Text("小组")
                 }.tag(2)
-            .onTapGesture {
-                    self.viewRouter.currentView = dashboard
-            }
             Text("The Last Tab")
                 .tabItem {
-                    Image(crown)
+                    Image(selection == 3 ? "crown-fill" : "crown")
                     Text("工具")
                 }.tag(3)
-            .onTapGesture {
-                    self.viewRouter.currentView = crown
-            }
             Text("The Last Tab")
                 .tabItem {
-                    Image(message)
+                    Image(selection == 4 ? "message-fill" : "message")
                     Text("信息")
                 }.tag(4)
-            .onTapGesture {
-                    self.viewRouter.currentView = message
-            }
             Text("The Last Tab")
                 .tabItem {
-                    Image(user)
+                    Image(selection == 5 ? "people-fill" : "people")
                     Text("我")
                 }.tag(5)
-            .onTapGesture {
-                    self.viewRouter.currentView = user
-            }
         }
         .font(.headline)
+        .foregroundColor(.black)
         .accentColor(.black)
         }
     }
@@ -69,4 +50,40 @@ struct ContentView_Previews: PreviewProvider {
         TopView().previewDevice("iPhone 11")
     }
 }
+
+struct ContentView: View {
+  @ObservedObject var webViewStore = WebViewStore()
+  
+  var body: some View {
+    NavigationView {
+      WebView(webView: webViewStore.webView)
+        .navigationBarTitle(Text(verbatim: webViewStore.webView.title ?? ""), displayMode: .inline)
+        .navigationBarItems(trailing: HStack {
+          Button(action: goBack) {
+            Image(systemName: "chevron.left")
+              .imageScale(.large)
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 32, height: 32)
+          }.disabled(!webViewStore.webView.canGoBack)
+          Button(action: goForward) {
+            Image(systemName: "chevron.right")
+              .imageScale(.large)
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 32, height: 32)
+          }.disabled(!webViewStore.webView.canGoForward)
+        })
+    }.onAppear {
+      self.webViewStore.webView.load(URLRequest(url: URL(string: "http://18.163.178.37/")!))
+    }
+  }
+  
+  func goBack() {
+    webViewStore.webView.goBack()
+  }
+  
+  func goForward() {
+    webViewStore.webView.goForward()
+  }
+}
+
 
