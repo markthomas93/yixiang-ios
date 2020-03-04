8bb5fafea61d69a9c2fc3a84c3ccfa6e2d8da815
hosxes <loli@h.fo>
2020-03-05 02:24:14 +0900

    ...

--- ScrollPaging.swift
+++ ScrollPaging.swift
@@ -1,26 +1,82 @@
 import SwiftUI
 
-struct ScrollViewPage : View {
-    @State var position : CGFloat = 0;
-    
-    var body : some View {
+struct Page: View, Identifiable {
+    let id = UUID()
+
+    var body: some View {
+        VStack(spacing: 0) {
+            ContentView()
+        }
+        .frame(width: 200, height: 300, alignment: .leading)
+        .background(getRandomColor())
+    }
+}
+
+struct SwiftUIPagerView<Content: View & Identifiable>: View {
+
+    @State private var index: Int = 0
+    @State private var offset: CGFloat = 0
+
+    var pages: [Content]
+
+    var body: some View {
         GeometryReader { geometry in
-            VStack {
-                Text("\(self.position)")
-                    .frame(width: 50, height: 30)
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 50) {
-                            Text("a")
-                        ContentView().frame(width: geometry.size.width, height: 704)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .center, spacing: 0) {
+                    ForEach(self.pages) { page in
+                        page
+                            .frame(width: geometry.size.width, height: nil)
                     }
                 }
             }
+            .content.offset(x: self.offset)
+            .frame(width: geometry.size.width, height: nil, alignment: .leading)
+            .gesture(DragGesture()
+                .onChanged({ value in
+                    self.offset = value.translation.width - geometry.size.width * CGFloat(self.index)
+                })
+                .onEnded({ value in
+                    if abs(value.predictedEndTranslation.width) >= geometry.size.width / 2 {
+                        var nextIndex: Int = (value.predictedEndTranslation.width < 0) ? 1 : -1
+                        nextIndex += self.index
+                        self.index = nextIndex.keepIndexInRange(min: 0, max: self.pages.endIndex - 1)
+                    }
+                    withAnimation { self.offset = -geometry.size.width * CGFloat(self.index) }
+                })
+            )
         }
     }
 }
 
-struct ScrollViewPage_Previews : PreviewProvider {
+extension Int {
+    func keepIndexInRange(min: Int, max: Int) -> Int {
+        switch self {
+            case ..<min: return min
+            case max...: return max
+            default: return self
+        }
+    }
+}
+
+func getRandomColor() -> Color {
+    let r = Double.random(in: 0..<1)
+    let g = Double.random(in: 0..<1)
+    let b = Double.random(in: 0..<1)
+    return Color(red: r, green: g, blue: b, opacity: 1.0)
+}
+
+struct SwiftUIPagerViewDemo: View {
+    var body: some View {
+        SwiftUIPagerView(
+            pages: (0..<4).map {
+                index in Page()
+        })
+    }
+}
+
+
+struct SwiftUIPagerViewDemo_Previews: PreviewProvider {
     static var previews: some View {
-        ScrollViewPage()
+        SwiftUIPagerViewDemo()
     }
 }
